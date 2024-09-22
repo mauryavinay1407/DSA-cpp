69de228e6d9b1a1fb8cb5377bad8ad8cf7948880
Vinay Maurya <121358775+mauryavinay1407@users.noreply.github.com>
2024-09-22 12:08:13 +0530

    Update Subarray_Sum_Equals_K.cpp

--- Sliding-window/Subarray_Sum_Equals_K.cpp
+++ Sliding-window/Subarray_Sum_Equals_K.cpp
@@ -1,6 +1,32 @@
 // problem:  https://leetcode.com/problems/subarray-sum-equals-k/description/
 
-/*
+/* 
+Approach-1: Brute Force
+Time Complexity: O(n^2)
+Space Complexity: O(1)
+*/
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            int sum = 0;
+            for (int j = i; j < nums.size(); j++) {
+                sum += nums[j];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
+
+
+/* 
+Approach-2: Optimal using prefix sum
 Time Complexity: O(n)
 Space Complexity: O(n)
 */
