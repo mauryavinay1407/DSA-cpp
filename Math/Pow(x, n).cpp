42ec9c46dbb5090a10fb3974347e0870cf7af815
Vinay Maurya <121358775+mauryavinay1407@users.noreply.github.com>
2024-10-08 17:28:38 +0530

    Create Pow(x, n).cpp

---
+++ Math/Pow(x, n).cpp
@@ -0,0 +1,42 @@
+// problem: https://leetcode.com/problems/powx-n/description/
+
+// Approach-1: Brute Force 
+
+class Solution {
+public:
+double myPow(double x, int n) {
+      double ans=1.0;
+      long long exponent=n;
+      if(exponent<0){
+          exponent = -exponent;
+           double temp=1.0;
+          for(long long i=0;i<exponent;i++)
+          temp*=x;
+          ans/=temp;
+      }
+      else{
+          for(long long i=0;i<exponent;i++)
+          ans*=x;
+      }
+      return ans;
+    }
+};
+
+// Approach-2: Optimal solution
+
+class Solution {
+public:
+    double findPow(double x,long n){
+        if(n==0)
+        return 1;
+        if(n<0)
+        return findPow(1/x,-n);
+        if(n%2==0)
+          return findPow(x*x,n/2);
+        else
+          return x*findPow(x*x,(n-1)/2);
+    }
+double myPow(double x, int n) {
+     return findPow(x,(long)n);
+    }
+};
