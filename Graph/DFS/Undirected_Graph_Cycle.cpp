9db14f2941470ae0e7cc4ee060aafa8b242c707d
Vinay Maurya <121358775+mauryavinay1407@users.noreply.github.com>
2024-10-12 22:55:27 +0530

    Update Undirected_Graph_Cycle.cpp

--- Graph/DFS/Undirected_Graph_Cycle.cpp
+++ Graph/DFS/Undirected_Graph_Cycle.cpp
@@ -1,6 +1,7 @@
 // problem :  https://www.geeksforgeeks.org/problems/detect-cycle-in-an-undirected-graph/1
 
 /*
+Approach-1: using DFS
 Time Complexity: (O(V + E))
 Space Complexity: (O(V))
 */
@@ -24,5 +25,47 @@ class Solution {
             return true;
        }
        return false;
+    }
+};
+
+/*
+Approach-2: using BFS
+Time Complexity: (O(V + E))
+Space Complexity: (O(V))
+*/
+
+class Solution {
+  public:
+     bool cyclicBFS(vector<int> adj[],int i,vector<bool>& visited){
+      queue<pair<int,int>> q;
+      q.push({i,-1});
+      visited[i]=true;
+      
+      while(!q.empty()){
+          int u=q.front().first;
+          int parent=q.front().second;
+          q.pop();
+         
+          for(auto& v:adj[u]){
+           if(!visited[v]){
+           visited[v]=true;
+            q.push({v,u});
+           }
+           else if(v != parent)
+            return true;
+          }
+      }
+      
+      return false;
+    }
+    
+    bool isCycle(int V, vector<int> adj[]) {
+      vector<bool> visited(V,false);
+      
+      for(int i=0;i<V;i++){
+          if(!visited[i] && cyclicBFS(adj,i,visited))
+          return true;
+      }
+      return false;
     }
 };
