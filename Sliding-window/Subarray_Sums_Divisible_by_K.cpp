15880988b8b66452c9665a4c3e0e9c919d7a80f5
Vinay Maurya <121358775+mauryavinay1407@users.noreply.github.com>
2024-10-15 01:43:45 +0530

    Update Subarray_Sums_Divisible_by_K.cpp

--- Sliding-window/Subarray_Sums_Divisible_by_K.cpp
+++ Sliding-window/Subarray_Sums_Divisible_by_K.cpp
@@ -33,23 +33,21 @@ Space Complexity:  O(min(n, k))
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
-      unordered_map<int,int> mp;
-      mp[0]=1;
-      int sum=0;
-      int count=0;
-      for(int i=0;i<nums.size();i++){
-       sum+=nums[i];
-       int rem=sum%k;
-       if(rem<0)
-       rem+=k;
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            sum += nums[i];
+            int rem = sum % k;
+            if (rem < 0)
+                rem += k;
 
-      if(mp.find(rem)!=mp.end()){
-        count+=mp[rem];    
-        mp[rem]++;
-      }
-      else
-      mp[rem]=1;
-     }
-     return count;
+            if (mp.find(rem) != mp.end())
+                count += mp[rem];
+                
+            mp[rem]++;
+        }
+        return count;
     }
 };
