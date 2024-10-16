2c04a1487888fcf3c3bb4dc48ed518658203d87f
Vinay Maurya <121358775+mauryavinay1407@users.noreply.github.com>
2024-10-17 01:46:59 +0530

    Create Split_Linked_List_Alternatingly.cpp

---
+++ Linked List/Split_Linked_List_Alternatingly.cpp
@@ -0,0 +1,36 @@
+// problem:  https://www.geeksforgeeks.org/problems/split-singly-linked-list-alternatingly/1
+
+/*
+Time Complexity: O(N)
+Space Complexity: O(1)
+*/
+
+    vector<Node*> alternatingSplitList(struct Node* head) {
+    if (!head) return {}; 
+    if (!head->next) return {head, nullptr}; 
+
+    Node* left = head;             
+    Node* right = head->next;       
+    Node* leftCurr = left;          
+    Node* rightCurr = right;        
+
+    Node* curr = head->next->next;  
+    bool addToLeft = true;          
+
+    while (curr) {
+        if (addToLeft) {
+            leftCurr->next = curr;  
+            leftCurr = leftCurr->next;
+        } else {
+            rightCurr->next = curr; 
+            rightCurr = rightCurr->next;
+        }
+        addToLeft = !addToLeft;     
+        curr = curr->next;          
+    }
+
+    leftCurr->next = nullptr;       
+    rightCurr->next = nullptr;      
+
+    return {left, right};           
+}
