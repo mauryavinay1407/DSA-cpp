999e2fc4eba316f4e04ac5ead2df1202cb9618f6
Vinay Maurya <vinaymaurya1407@gmail.com>
2025-01-18 08:46:19 +0530

    Create Reverse_a_linked_list.cpp

---
+++ Linked List/Reverse_a_linked_list.cpp
@@ -0,0 +1,22 @@
+// problem : https://www.geeksforgeeks.org/problems/reverse-a-linked-list/1
+
+/*
+Approach: use 3 pointers
+Time Complexity  :  O(N)
+Space Complexity :  O(1)
+*/
+class Solution {
+public:
+    Node* reverseList(struct Node* head) {
+        Node* pre = NULL;
+        Node* cur = head;
+        Node* nxt = head;
+        while(cur){
+            nxt = nxt -> next;
+            cur->next = pre;
+            pre= cur;
+            cur = nxt;
+        }
+        return pre;
+    }
+};
