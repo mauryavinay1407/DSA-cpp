387bfb5aa661170d3e1405938bbbdec93f5fa0e5
Vinay Maurya <vinaymaurya1407@gmail.com>
2025-02-20 22:19:02 +0530

    Update Find_median_in_a_stream.cpp

--- Heap/Find_median_in_a_stream.cpp
+++ Heap/Find_median_in_a_stream.cpp
@@ -65,3 +65,43 @@ class Solution {
         return result;
     }
 };
+
+
+/*
+Approach-3 : Optimal using Heap
+Time Complexity    :   O(N*logN)
+Spacd Complexity   :   O(1)
+*/
+class Solution {
+  public:
+    vector<double> getMedian(vector<int> &arr) {
+       vector<double> result;
+        priority_queue<int> maxHeap; // Left part (lower half)
+        priority_queue<int, vector<int>, greater<int>> minHeap; // Right part (higher half)
+
+        for (int num : arr) {
+            if (maxHeap.empty() || num <= maxHeap.top()) {
+                maxHeap.push(num);
+            } else {
+                minHeap.push(num);
+            }
+
+            // Balance the heaps
+            if (maxHeap.size() > minHeap.size() + 1) {
+                minHeap.push(maxHeap.top());
+                maxHeap.pop();
+            } else if (minHeap.size() > maxHeap.size()) {
+                maxHeap.push(minHeap.top());
+                minHeap.pop();
+            }
+
+            // Get median
+            if (maxHeap.size() == minHeap.size()) {
+                result.push_back((maxHeap.top() + minHeap.top()) / 2.0);
+            } else {
+                result.push_back(maxHeap.top());
+            }
+        }
+        return result;
+    }
+};
