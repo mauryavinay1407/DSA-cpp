ddc0913c4a922cc4a1a43a7aa812b8c095ad0228
Vinay Maurya <vinaymaurya1407@gmail.com>
2025-02-24 16:55:46 +0530

    Create Next_Greater_Element.cpp

---
+++ Stack/Tree/Next_Greater_Element.cpp
@@ -0,0 +1,26 @@
+// problem  :  https://www.geeksforgeeks.org/problems/next-larger-element-1587115620/1
+
+/*
+Approach- Using Stack
+Time Complexity       :   O(N)
+Space Complexity      :   O(N)
+*/
+
+
+class Solution {
+public:
+    vector<int> nextLargerElement(vector<int>& arr) {
+        int n = arr.size();
+        vector<int> result(n,-1);
+        stack<int> st;
+        result[n-1] = -1;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && arr[st.top()] < arr[i]){
+                result[st.top()] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return result;
+    }
+};
