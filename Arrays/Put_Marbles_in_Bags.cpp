66abd416a9a17e118be7f677be8b4651d8eaddda
Vinay Maurya <vinaymaurya1407@gmail.com>
2025-03-31 20:00:16 +0530

    Create Put_Marbles_in_Bags.cpp

---
+++ Arrays/Put_Marbles_in_Bags.cpp
@@ -0,0 +1,27 @@
+// problem : https://leetcode.com/problems/put-marbles-in-bags
+
+/*
+Approach - By finding pair
+Time Complexity     : O(N*logN)
+Space Complexity    : O(N)
+*/
+
+class Solution {
+public:
+    long long putMarbles(vector<int>& weights, int k) {
+        int n = weights.size();
+
+        vector<int> pairSum(n - 1, 0);
+        for (int i = 0; i < n - 1; i++) {
+            pairSum[i] = weights[i] + weights[i + 1];
+        }
+        sort(pairSum.begin(), pairSum.end());
+        long maxSum = 0;
+        long minSum = 0;
+        for (int i = 0; i < k - 1; i++) {
+            minSum += pairSum[i];
+            maxSum += pairSum[n - i - 2];
+        }
+        return maxSum - minSum;
+    }
+};
