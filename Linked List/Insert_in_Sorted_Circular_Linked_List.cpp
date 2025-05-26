111ac3adb2d2fba2a1a7c8d6023f059d34810a0f
Vinay Maurya <vinaymaurya1407@gmail.com>
2025-05-26 20:52:24 +0530

    Create Insert_in_Sorted_Circular_Linked_List.cpp

---
+++ Linked List/Insert_in_Sorted_Circular_Linked_List.cpp
@@ -0,0 +1,67 @@
+// problem  : https://www.geeksforgeeks.org/problems/sorted-insert-for-circular-linked-list/1
+
+/*
+Approach - Using simple traversal
+Time Complexity       :  O(N)
+Space Complexity      :  O(1)
+*/
+
+class Solution {
+  public:
+    Node* sortedInsert(Node* head, int data) {
+        // code here
+         Node* newNode = new Node(data); 
+    
+        // If linked list is empty
+        if (head == nullptr) {
+            newNode->next = newNode; 
+            head = newNode;
+            return head;
+        }
+        
+        Node* curr = head;
+        Node* nextToCurr = head->next;
+        
+        // Insert at the beginning if data is less 
+      	// than or equal to head's data
+        if (data <= head->data) {
+            Node* lastNode = head;
+            while (lastNode->next != head) {
+              	
+              	// Find the last node
+                lastNode = lastNode->next; 
+            }
+          
+          	// Set new node's next to head
+            newNode->next = head; 
+            
+          	// Update last node's next to new node
+          	lastNode->next = newNode; 
+            
+          	// Update head to new node
+          	head = newNode; 
+            return head;
+        }
+        
+        // Insert in the middle of the list
+        while (curr->next != head) {
+            if (curr->data < data && nextToCurr->data >= data) {
+              	
+              	// Set new node's next to current's next
+                newNode->next = curr->next; 
+                curr->next = newNode;
+                return head;
+            } else {
+              
+              	// Move to the next node
+                curr = curr->next; 
+                nextToCurr = nextToCurr->next; 
+            }
+        }
+        
+        // Insert at the end of the list
+        newNode->next = head; 
+        curr->next = newNode; 
+        return head;
+    }
+};
