53b57dd89219386a322b0cccd37188d22d5f9bb1
Vinay Maurya <vinaymaurya1407@gmail.com>
2025-06-08 12:43:41 +0530

    Update Lexicographical_Numbers.cpp

--- Graph/DFS-BFS/Lexicographical_Numbers.cpp
+++ Graph/DFS-BFS/Lexicographical_Numbers.cpp
@@ -20,3 +20,35 @@ public:
         return ans;
     }
 };
+
+/*
+Approach-2 : Using Simple loop
+Time Complexity       : O(N)
+Space Complexity      : O(1)
+*/
+class Solution {
+public:
+    vector<int> lexicalOrder(int n) {
+        vector<int> result(n);
+
+        int curr = 1;
+
+        for(int i = 0; i < n; i++){
+            result[i] = curr;
+
+            if(curr * 10 <= n){
+                curr *= 10;
+            }
+            else{
+                if(curr == n){
+                    curr /= 10;
+                }
+                curr++;
+                while(curr % 10 == 0){
+                    curr /= 10;
+                }
+            }
+        }
+        return result;
+    }
+};
