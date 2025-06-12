303d37536bd38b825741c30ea848e272e97ac592
Vinay Maurya <vinaymaurya1407@gmail.com>
2025-06-12 11:44:40 +0530

    Create K_closest_elements.cpp

---
+++ Heap/K_closest_elements.cpp
@@ -0,0 +1,45 @@
+//  problem  :  https://www.geeksforgeeks.org/problems/k-closest-elements3619/1
+
+/*
+Approach - Using max heap
+Time Complexity      : O(N*logk + klogk)
+Space Complexity     : O(1)
+*/
+class Solution {
+public:
+    using P = pair<int,int>;
+    
+    struct comparator{
+        bool operator()(const P& a,const P& b){
+            if(a.first == b.first){
+                return a.second > b.second;
+            }
+            return a.first < b.first;
+        }
+    };
+    vector<int> printKClosest(vector<int> arr, int k, int x) {
+        // Code here
+        priority_queue<P,vector<P>,comparator> maxHeap;
+        
+        for(int i=0;i<arr.size();i++){
+            int diff = abs(x - arr[i]);
+            
+            if(diff){
+                maxHeap.push({diff,arr[i]});
+            }
+            
+            if(maxHeap.size() > k){
+                maxHeap.pop();
+            }
+        }
+        
+        vector<int> result(k);
+        int i = k - 1;
+        while(!maxHeap.empty()){
+            result[i--] = maxHeap.top().second;
+            maxHeap.pop();
+        }
+        
+        return result;
+    }
+};
