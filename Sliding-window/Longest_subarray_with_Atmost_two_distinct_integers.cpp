6d979fe3ababb51e0f8c2b548cb0282a727f3036
Vinay Maurya <vinaymaurya1407@gmail.com>
2025-07-02 19:48:49 +0530

    Create Longest_subarray_with_Atmost_two_distinct_integers.cpp

---
+++ Sliding-window/Longest_subarray_with_Atmost_two_distinct_integers.cpp
@@ -0,0 +1,33 @@
+// problem : https://www.geeksforgeeks.org/problems/fruit-into-baskets-1663137462/1
+
+/*
+Approach - Using unordered map + Sliding window
+Time Complexity      :  O(N)
+Space Complexity     :  O(N)
+*/
+class Solution {
+  public:
+    int totalElements(vector<int> &arr) {
+        // code here
+        unordered_map<int,int> mp;
+        
+        int result = 0;
+        int j = 0;
+        for(int i = 0; i < arr.size(); i++){
+            mp[arr[i]]++;
+            
+            if(mp.size() <= 2){
+                result = max(result, i - j + 1);
+            }
+            
+            while(mp.size() > 2){
+                mp[arr[j]]--;
+                if(mp[arr[j]] == 0){
+                    mp.erase(arr[j]);
+                }
+                j++;
+            }
+        }
+        return result;
+    }
+};
