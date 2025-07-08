7b9d16abc40d7e6c31a6096661256b436e599bac
Vinay Maurya <vinaymaurya1407@gmail.com>
2025-07-08 20:58:05 +0530

    Create Next_element_with_greater_frequency.cpp

---
+++ Stack/Next_element_with_greater_frequency.cpp
@@ -0,0 +1,32 @@
+// problem : https://www.geeksforgeeks.org/problems/next-element-with-greater-frequency--170637/1
+
+/*
+Approach - Using hashmap + stack
+Time Complexity        : O(N)
+Space Complexity       : O(N)
+*/
+class Solution {
+  public:
+    vector<int> findGreater(vector<int>& arr) {
+          // code here
+        unordered_map<int, int> freq;
+        for(auto i : arr){
+            freq[i]++;
+        }
+        int size = arr.size();
+        stack<int> st;
+        vector<int> result(size, -1);
+        for(int i = size - 1; i >= 0; i--){
+            while(!st.empty() && freq[st.top()] <= freq[arr[i]]){
+                st.pop();
+            }
+            
+            if(!st.empty()){
+                result[i] = st.top();
+            }
+            
+            st.push(arr[i]);
+        }
+        return result;
+    }
+};
