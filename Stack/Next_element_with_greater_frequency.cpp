72e4ce9030ad964411ad17ca3b2402d8e79de92c
Vinay Maurya <vinaymaurya1407@gmail.com>
2025-07-09 19:28:43 +0530

    Update Next_element_with_greater_frequency.cpp

--- Stack/Next_element_with_greater_frequency.cpp
+++ Stack/Next_element_with_greater_frequency.cpp
@@ -30,3 +30,26 @@ class Solution {
         return result;
     }
 };
+//////////////////////////////////////////// OR ////////////////////////////////////////////////////////////
+// my simple template(with same time and space) :
+class Solution {
+  public:
+    vector<int> findGreater(vector<int>& arr) {
+        // code here
+        int n = arr.size();
+        unordered_map<int,int> mp;
+        for(auto& num : arr){
+            mp[num]++;
+        }
+        vector<int> result(n, -1);
+        stack<int> st;
+        for(int i = 0; i < n; i++){
+            while(!st.empty() && mp[arr[i]] > mp[arr[st.top()]]){
+                result[st.top()] = arr[i];
+                st.pop();
+            }
+            st.push(i);
+        }
+        return result;
+    }
+};
