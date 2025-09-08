083ca4e497f0b6586f1e5ead193ee94d1d84c35a
Vinay Maurya <vinaymaurya1407@gmail.com>
2025-09-08 23:39:53 +0530

    Create Merge_k_sorted_linked_lists.cpp

---
+++ Linked List/Merge_k_sorted_linked_lists.cpp
@@ -0,0 +1,37 @@
+// problem : https://www.geeksforgeeks.org/problems/merge-k-sorted-linked-lists/1
+
+
+/*
+Approach :  Using min-heap
+Time Complexity     : O(N*logk) 
+Space Complexity    : O(k) 
+*/
+class Solution {
+  public:
+static bool cmp(Node *first, Node *second)
+    {
+        return first->data > second->data;
+    }
+    
+    Node* mergeKLists(vector<Node*>& arr) 
+    {
+        priority_queue<Node *, vector<Node*>, decltype(&cmp)> pq(cmp);
+        Node *dummy = new Node(0);
+        Node *tail = dummy;
+        for(Node *temp: arr)
+        {
+            if(temp)
+                pq.push(temp);    
+        }
+        while(pq.size())
+        {
+            Node *smallest = pq.top();
+            pq.pop();
+            tail->next = smallest;
+            tail = tail->next;
+            if(smallest->next != NULL)
+                pq.push(smallest->next);
+        }
+        return dummy->next;
+    }
+};
