fe70f279a9d4e77781e8fa872a725c4ce4aec38a
Vinay Maurya <vinaymaurya1407@gmail.com>
2025-09-12 23:15:47 +0530

    Create Vowels_Game_in_a_String.cpp

---
+++ Math/Vowels_Game_in_a_String.cpp
@@ -0,0 +1,38 @@
+// problem :  https://leetcode.com/problems/vowels-game-in-a-string
+
+
+/*
+Approach-1   Simple math
+Time Complexity :  O(n) 
+Space Complexity:  O(1) 
+*/
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+
+        for(char &ch : s) {
+            if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
+
+
+/*
+Approach-2 Using one liner STL
+Time Complexity  : O(n)
+Space Complexity : O(1)
+*/
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        auto lambda = [](char ch) {
+            return string("aeiou").find(ch) != string::npos;
+        };
+
+        return any_of(s.begin(), s.end(), lambda);
+    }
+};
